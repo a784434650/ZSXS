0373fc82fab9eeb0eaca3854adf2f530ab318d58
a784434650 <784434650@qq.com>
2017-07-03 17:24:48 +0800

    1

--- app/src/main/java/com/huida/zsxs/activity/AudioActivity.java
+++ app/src/main/java/com/huida/zsxs/activity/AudioActivity.java
@@ -3,6 +3,7 @@ package com.huida.zsxs.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -13,7 +14,9 @@ import com.huida.zsxs.R;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
-public class AudioActivity extends Activity {
+import static com.huida.zsxs.R.mipmap.back;
+
+public class AudioActivity extends Activity implements View.OnClickListener{
 
     @InjectView(R.id.ib_back)
     ImageButton ibBack;
@@ -41,5 +44,15 @@ public class AudioActivity extends Activity {
     private void initData() {
     }
     private void initListener() {
+        ibBack.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.ib_back:
+                finish();
+                break;
+        }
     }
 }
